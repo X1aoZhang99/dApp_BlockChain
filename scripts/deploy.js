946d63658d8a94160488adcf292474c60c1ce63b
Xiao Zhang <zhang.xiao12@northeastern.edu>
2023-08-22 16:26:26 -0400

    Ready for deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,11 +8,33 @@ const hre = require("hardhat")
 const { items } = require("../src/items.json")
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
+  return ethers.utils.parseUnits(n.toString(), 'ether');
 }
 
 async function main() {
+  // Setup Accounts
+  const [deployer] = await ethers.getSigners();
 
+  // Deploy DApp
+  const Dappazon = await ethers.getContractFactory("Dappazon");
+  const dappazon = await Dappazon.deploy();
+  await dappazon.deployed();
+  console.log("dApp deployed to:", dappazon.address);
+
+  // List Items
+  for (let i = 0; i < items.length; i++) {
+    const transaction = await dappazon.connect(deployer).list(
+      items[i].id,
+      items[i].name,
+      items[i].category,
+      items[i].image,
+      tokens(items[i].price),
+      items[i].rating,
+      items[i].stock
+    );
+    await transaction.wait();
+    console.log(`Listed item ${items[i].id}: ${items[i].name}`);
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
